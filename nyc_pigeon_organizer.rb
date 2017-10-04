865dac7ad4d3897b6b1303b99f6b827efd69ae69
github username <github email address>
2017-10-03 21:30:13 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,30 @@
-def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |top_level_hash, bottom_level_hash|
+def nyc_pigeon_organizer(original_data)
+  name_hash = {}
+  original_data.each do |top_level_hash, bottom_level_hash|
     bottom_level_hash.each do |key, value|
       value.each do |another_value|
-        new_hash[another_value] = {}
+        name_hash[another_value] = {}
       end
     end
   end
-  new_hash
+  name_hash.each do |orgnanized_name, organized_hash|
+    original_data.each do |top_level_hash, bottom_level_hash|
+      organized_hash[top_level_hash] = []
+    end
+  end
+  name_hash.each do |orgnanized_name, organized_hash|
+    organized_hash.each do |organized_stat, organized_value|
+      original_data.each do |top_level_hash2, bottom_level_hash2|
+          bottom_level_hash2.each do |key, array|
+            array.each do |value|
+              if value == orgnanized_name
+                if organized_stat == top_level_hash2
+                  name_hash[orgnanized_name][organized_stat] << key.to_s
+                end
+              end
+            end
+          end
+      end
+    end
+  end
 end
